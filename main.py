ce3d9f588bfe609824786bf722db267b18d32e78
Aaryan Patel <aaryanpatel683@gmail.com>
2023-08-01 22:10:58 +0530

    delete garbage

--- main.py
+++ main.py
@@ -3,72 +3,65 @@ from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandl
 from webScraping import getAllLinks
 import json
 import os
+from dotenv import load_dotenv
 
-bot_uri = os.environ.get('TELEGRAM_BOT_URI')
+# load the envoironment keys
+load_dotenv()
 
 
 def trucateString(original_string):
-    max_length = 45  # Maximum length of the final string (including '...')
-
+    max_length = 45
     if len(original_string) > max_length:
-        # Calculate the length of the truncated portion on each side of '...'
         truncated_length = (max_length - 3) // 2
-        # Create the truncated string with '...' in the middle
         truncated_string = original_string[:truncated_length] + \
             "..." + original_string[-truncated_length:]
         return truncated_string
     else:
-        # If the original string is not longer than the maximum length, keep it as it is
         return original_string
 
+
 # start function for bot
-
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     await update.message.reply_html("Great to see you here!\n\nType subject name for searching question papers.")
 
+
 # help command for bot
-
-
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
     await update.message.reply_text("Help!")
 
 
-# sending message to user
+# getting the subject name from the user and sending the links
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
-    # Your lists
+
+    # get all links from the website related to the subject
     arr = await getAllLinks(update.message.text)
+
+    # if the arr is string then send the string
     if type(arr) == str:
         await update.message.reply_text(arr)
         return
 
     try:
-        # Create a list of InlineKeyboardButton for each item in list1
         keyboard_list = [
             [InlineKeyboardButton(trucateString(item), callback_data=item)] for item in arr if len(item) < 64
         ]
-
-        # Create the InlineKeyboardMarkup
         reply_markup = InlineKeyboardMarkup(keyboard_list)
 
-        # Send the message with the options
         await update.message.reply_text("Please select the paper to download:",
                                         reply_markup=reply_markup)
 
     except:
         await update.message.reply_text("No links found!")
 
-    # await update.message.reply_document("https://www.rgpvonline.com/be/it-303-data-structure-nov-2022.pdf")
 
-
+# sending the pdf to the user
 async def handle_selection(update, context):
     query = update.callback_query
     selected_item = "https://www.rgpvonline.com/be/"+query.data+".pdf"
-    print(selected_item)
-    # Handle the selected item
+
     try:
         await query.message.reply_document(selected_item)
     except:
@@ -79,8 +72,7 @@ def main() -> None:
     """Start the bot."""
 
     application = Application.builder().token(
-        bot_uri).build()
-    # application = Application.builder().token(os.getenv('TELEGRAM_KEY')).build()
+        os.getenv('TELEGRAM_BOT_TOKEN')).build()
 
     # start command
     application.add_handler(CommandHandler("start", start))
--- webScraping.py
+++ webScraping.py
@@ -29,38 +29,3 @@ async def getAllLinks(search_text):
             return matching_items
     else:
         return "No links found!"
-
-
-def getSingleLink(target_url):
-
-    common_part = "/".join(target_url.split("/")[:4])
-
-    response = requests.get(target_url)
-    if response.status_code != 200:
-        print(
-            f"Failed to fetch data from {paperPage}. Status code: {response.status_code}")
-        return []
-
-    soup = BeautifulSoup(response.text, "html.parser")
-    allUrls = soup.find_all("a")
-
-    try:
-        downloadLink = next((url for url in allUrls if url.get(
-            "href") == target_url.split("/")[4].replace("html", "pdf")), None)
-    except:
-        return None
-
-    fullDownloadLink = common_part + "/" + downloadLink.get("href")
-    return fullDownloadLink
-
-
-# if __name__ == "__main__":
-
-#     links = getAllLinks(
-#         "https://www.rgpvonline.com/btech-it-question-papers.html#list")
-
-    # url = getSingleLink(
-    #     "https://www.rgpvonline.com/be/it-833-artificial-intelligence-jun-2011.html")
-    # print(url)
-
-# rl = "https://www.rgpvonline.com/be/it-3004-object-oriented-programming-and-methodology-jun-2020.html"
